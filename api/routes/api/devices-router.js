009e2915a5345c47d129c74ff0a7da069d818a09
Luis Bolanos <luis.bolanos01@telusinternational.com>
2023-05-04 16:41:02 -0700

    add updating permissions for aws user

--- api/routes/api/devices-router.js
+++ api/routes/api/devices-router.js
@@ -9,4 +9,4 @@ const NEW_DEVICE_ROUTE = "/addDevice";
 router.get(ALL_DEVICES_ROUTE, DevicesController.showAll);
 router.post(NEW_DEVICE_ROUTE, DevicesController.newDevice);
 
-module.exports = router;  
+module.exports = router;
